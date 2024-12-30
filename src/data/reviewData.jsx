4f9948729c6c917a74389f6f9cc4087ed91697a4
Nimsara Thisal Paramulla <85945422+nimsara1999@users.noreply.github.com>
2024-12-31 02:43:15 +0530

    Implemented review and feedback section

---
+++ src/data/reviewData.jsx
@@ -0,0 +1,35 @@
+
+export const reviewData = [
+  {   name:"Kevina2",
+      url: "https://raw.githubusercontent.com/nimsara1999/reviews/refs/heads/main/kevin2.png"
+  },
+  {
+      name:"Omatagalpa",
+      url:"https://raw.githubusercontent.com/nimsara1999/reviews/refs/heads/main/omatagalpa.png"
+  },
+  {
+      name:"diroshan",
+      url:"https://raw.githubusercontent.com/nimsara1999/reviews/refs/heads/main/diroshan.png"
+  },
+  {
+      name:"kimyon",
+      url:"https://raw.githubusercontent.com/nimsara1999/reviews/refs/heads/main/kimyon.png"
+  },
+  {
+      name:"Kevin1",
+      url:"https://raw.githubusercontent.com/nimsara1999/reviews/refs/heads/main/kevin1.png"
+  },
+  {
+      name:"Yenushim",
+      url:"https://raw.githubusercontent.com/nimsara1999/reviews/refs/heads/main/yenushim.png"
+  },
+  {
+      name:"Upwork1",
+      url:"https://raw.githubusercontent.com/nimsara1999/reviews/refs/heads/main/upwork1.png"
+  },
+  {
+      name:"Rohan",
+      url:"https://raw.githubusercontent.com/nimsara1999/reviews/refs/heads/main/rohan.png"
+  }
+];
+
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,10 @@
-import React, { Component } from 'react';
+import React, { Component,img } from 'react';
 import './Home.css';  // Import the CSS file
 import AboutCard from '../components/AboutCard';
 import ProjectCard from '../components/ProjectCard';
 import aboutData from '../data/AboutData'; // Import the aboutData array
 import projectData from '../data/ProjectData'; // Import the projectData array
+import {reviewData} from '../data/reviewData';
 import '../components/ProjectCard.css'; // Import the CSS file
 import { BsGithub } from "react-icons/bs";
 import { FaLinkedin } from "react-icons/fa6";
@@ -11,7 +12,7 @@ import { TbBrandFiverr } from "react-icons/tb";
 import { FaSquareUpwork } from "react-icons/fa6";
 import { BsEnvelope,BsPhone } from "react-icons/bs"; // Import the email icon
 import { useInView } from 'react-intersection-observer';
-import { ScrollSpy } from 'bootstrap'; // Import ScrollSpy from bootstrap
+import { Carousel, ScrollSpy } from 'bootstrap'; // Import ScrollSpy from bootstrap
 import Chatbot from '../components/ChatBot/Chatbot';
 import { Typewriter, Cursor } from 'react-simple-typewriter';
 import GitHubCalendar from '../components/GithubCalendar';
@@ -25,7 +26,7 @@ const about1 = (
 
 const about2 = (
     <p className="text-light-grey" style={{textAlign:'justify',marginRight:5}}>
-        I hold a <span className="tech-highlight">BSc Engineering Honours Degree in Computer Science and Engineering from the University of Moratuwa, Sri Lanka</span>, specializing in Integrated Computer Engineering. My professional experience includes a role as a Trainee <span className="tech-highlight">Software Engineer at Circles.Life Technologies</span> and a <span className="tech-highlight">5 star rating on Fiverr and Upwork platforms</span>, underscoring my expertise across various technical domains.
+        I hold a <span className="tech-highlight">BSc Engineering Honours Degree in Computer Science and Engineering from the University of Moratuwa, Sri Lanka</span>, specializing in Integrated Computer Engineering. My professional experience includes a role as a Trainee <span className="tech-highlight">Software Engineer at Circles.Life Technologies</span> and a <span className="tech-highlight">5 star rating on Fiverr and Upwork freelancing platforms</span>, underscoring my expertise across various technical domains.
     </p>
 );
 
@@ -123,9 +124,9 @@ class Home extends Component {
                             words={['Full-stack Engineer', 'Embedded Systems Engineer']}
                             loop={0}
                             cursor
-                            cursorStyle='|'
-                            typeSpeed={100}
-                            deleteSpeed={100}
+                            cursorStyle='_'
+                            typeSpeed={120}
+                            deleteSpeed={120}
                             delaySpeed={1000}
                         />
                     </h5>
@@ -133,11 +134,11 @@ class Home extends Component {
                     <img src="https://drive.google.com/thumbnail?id=1xZeEK-zVG4JbZasJodlxg3Xcs26UtFuo" className="img-fluid double-size mt-3"
                         style={{ borderRadius: '50%',  boxShadow: '0 4px 4px rgba(0, 0, 0, 0.1)', backgroundColor: 'rgba(247, 247, 247, 0.1)' }}
                     />
+                    
                     <div className="mt-5"/>
 
                     <div id="list-example">
                         <a className="list-group-item list-group-item-action mb-2" href="#list-item-1" style={currentID==='list-item-1'?{color:'white', marginLeft:15}:{color:'#959caf'}}>- About</a>
-                        <a className="list-group-item list-group-item-action mb-2" href="#list-item-2" style={currentID==='list-item-2'?{color:'white',marginLeft:15}:{color:'#959caf'}}>- Background</a>
                         <a className="list-group-item list-group-item-action mb-2" href="#list-item-3" style={currentID==='list-item-3'?{color:'white',marginLeft:15}:{color:'#959caf'}}>- Projects</a>
                         <a className="list-group-item list-group-item-action mb-2" href="#list-item-4" style={currentID==='list-item-4'?{color:'white',marginLeft:15}:{color:'#959caf'}}>- Contact</a>
                     </div>
@@ -233,9 +234,11 @@ class Home extends Component {
                         {about1}
                         {about2}
                         {about3}
-                        </VisibleDiv>
-                        <VisibleDiv className="mt-5" id="list-item-2" handleVisibilityChange={this.handleVisibilityChange}/>
-                            <h4 className='mt-5 mb-3'>Background -</h4>
+                        
+                        
+                        
+                            <h4 className='mt-5 mb-3'>Background</h4>
+                            </VisibleDiv>
                             <div className="about-cards-container">
                                 {aboutData.map((about, index) => (
                                     <div className="about-card">
@@ -247,9 +250,9 @@ class Home extends Component {
 
                             
 
-                        <VisibleDiv className="mt-5" id="list-item-3" handleVisibilityChange={this.handleVisibilityChange}/>
-                        <h4 className='mt-7'>Projects & Research -</h4>
-                
+                        <VisibleDiv className="mt-5" id="list-item-3" handleVisibilityChange={this.handleVisibilityChange}>
+                        <h4 className='mt-7'>Projects & Research</h4>
+                        </VisibleDiv>
                             <GitHubCalendar/>
 
                             <div className="dropdown mb-3">
@@ -269,11 +272,36 @@ class Home extends Component {
                             <ProjectCard key={index} {...project} selectedType={this.state['selectedType']} />
                         ))}
 
+
+                        <VisibleDiv className="mt-5" id="list-item-5" handleVisibilityChange={this.handleVisibilityChange}></VisibleDiv>
+                        <h4 className='mt-7 mb-4'>Reviews and Feedback</h4>
+
+                                    <div id="carouselExampleRide" class="carousel carousel-dark slide ms-1 me-4" data-bs-ride="true">
+                                    <div className="carousel-inner me-4">
+                                        <div class="carousel-item active" data-bs-interval="4000"> <img src={reviewData[0].url} class="d-block rounded w-100" alt="..."/></div>
+                                        <div class="carousel-item" data-bs-interval="4000"> <img src={reviewData[1].url} class="d-block rounded w-100" alt="..."/></div>
+                                        <div class="carousel-item" data-bs-interval="4000"> <img src={reviewData[2].url} class="d-block rounded w-100" alt="..."/></div>
+                                        <div class="carousel-item" data-bs-interval="4000"> <img src={reviewData[3].url} class="d-block rounded w-100" alt="..."/></div>
+                                        <div class="carousel-item" data-bs-interval="4000"> <img src={reviewData[4].url} class="d-block rounded w-100" alt="..."/></div>
+                                        <div class="carousel-item" data-bs-interval="4000"> <img src={reviewData[5].url} class="d-block rounded w-100" alt="..."/></div>
+                                        <div class="carousel-item" data-bs-interval="4000"> <img src={reviewData[6].url} class="d-block rounded w-100" alt="..."/></div>
+                                        <div class="carousel-item" data-bs-interval="4000"> <img src={reviewData[7].url} class="d-block rounded w-100" alt="..."/></div>
+                                    </div>
+                                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleRide" data-bs-slide="prev">
+                                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                        <span class="visually-hidden">Previous</span>
+                                    </button>
+                                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleRide" data-bs-slide="next">
+                                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                        <span class="visually-hidden">Next</span>
+                                    </button>
+                                    </div>
+
                         <VisibleDiv className="mt-5" id="list-item-4" handleVisibilityChange={this.handleVisibilityChange}>
-                        <h4 className='mt-7 mb-3'>Contact Me -</h4>
+                        <h4 className='mt-7 mb-3'>Contact Me</h4>
                         <p>Email: nimsarathisalgcc@gmail.com</p>
                         <p>Mobile: +94710880133</p>
-                        <p>Address: Madampe, Atakalanpanna, Sri Lanka.</p>
+                        <p>Address: Madampe, Ratnapura, Sri Lanka.</p>
                         <div className="d-flex col-5 text-center mb-6">
                         <a className="list-group-item list-group-item-action icon-link github" href="https://github.com/nimsara1999?tab=repositories">
                             <BsGithub style={{fontSize:20}} />
